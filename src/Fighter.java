275e61a92be37d1452aa05a001b006f033a11b7c
DaniloTosic <danilotosic@danilotosic.home>
2025-10-23 02:21:41 +0200

    Second class

--- src/Fighter.java
+++ src/Fighter.java
@@ -6,6 +6,39 @@ public class Fighter {
 	private int strength;
 	private int defence;
 	private int health;
+	private int numberOfCredits;
+	private boolean inGuard;
+	private boolean inAir;
+	
+	public Fighter(String name, int speed, int strength, int defence, int health, int numberOfCredits, boolean inGuard, boolean inAir) {
+		this.name = name;
+		this.speed = speed;
+		this.strength = strength;
+		this.defence = defence;
+		this.health = health;
+		this.numberOfCredits = numberOfCredits;
+		this.inGuard = inGuard;
+		this.inAir = inAir;
+	}
+	
+	public Fighter(String name, int speed, int strength, int defence, int health, int numberOfCredits, boolean inGuard) {
+		this.name = name;
+		this.speed = speed;
+		this.strength = strength;
+		this.defence = defence;
+		this.health = health;
+		this.numberOfCredits = numberOfCredits;
+		this.inGuard = inGuard;
+	}
+	
+	public Fighter(String name, int speed, int strength, int defence, int health, int numberOfCredits) {
+		this.name = name;
+		this.speed = speed;
+		this.strength = strength;
+		this.defence = defence;
+		this.health = health;
+		this.numberOfCredits = numberOfCredits;
+	}
 	
 	public Fighter(String name, int speed, int strength, int defence, int health) {
 		this.name = name;
@@ -13,6 +46,16 @@ public class Fighter {
 		this.strength = strength;
 		this.defence = defence;
 		this.health = health;
+		this.inGuard = false;
+	}
+	
+	public Fighter() {
+		this.name = "";
+		this.speed = 0;
+		this.strength = 0;
+		this.defence = 0;
+		this.health = 0;
+		this.numberOfCredits = 1;
 	}
 
 	public String getName() {
@@ -63,10 +106,34 @@ public class Fighter {
 		System.out.println(this.getName());
 	}
 	
+	public int getNumberOfCredits() {
+		return numberOfCredits;
+	}
+
+	public void setNumberOfCredits(int numberOfCredits) {
+		this.numberOfCredits = numberOfCredits;
+	}
+
+	public boolean isInGuard() {
+		return inGuard;
+	}
+
+	public void setInGuard(boolean inGuard) {
+		this.inGuard = inGuard;
+	}
+	
+	public boolean isInAir() {
+		return inAir;
+	}
+
+	public void setInAir(boolean inAir) {
+		this.inAir = inAir;
+	}
+
 	public void fighterDeath() {
-		System.out.println("YOur char has died");
+		System.out.println("Char has died.");
 		this.setHealth(0);
-		System.out.println(this.getHealth());
+		// System.out.println(this.getHealth());
 	}
 
 	@Override
@@ -75,5 +142,53 @@ public class Fighter {
 				+ ", health=" + health + "]";
 	}
 	
+	public void moveRight() {
+		System.out.println("Your char moved to the right. ");
+	}
 	
+	public void moveLeft() {
+		System.out.println("Your char moved to the left. ");
+	}
+	
+	public void jump() {
+		System.out.println("Your char jumped ");
+		this.setInAir(true);
+	}
+	
+	public void land() {
+		System.out.println("Your char landed.");
+		this.setInAir(false);
+	}
+	
+	public void crouch() {
+		System.out.println("You crouched");
+	}
+	
+	public void getInGuard() {
+		this.setInGuard(true);
+	}
+	
+	public void leaveGuard() {
+		this.setInGuard(false);
+	}
+	
+	public int firstKick(Fighter opponent) {
+		if(this.isInGuard()) {
+			return 0;
+		}
+		if(opponent.isInGuard()) {
+			System.out.println("Your opponent defended.");
+			return 0;
+		}else {
+			System.out.println("You dealt small damage");
+			return 10;
+		}
+	}
+	
+	public void calculateDamage(int damage, Fighter f1) {
+		f1.setHealth(f1.getHealth()-damage);
+		if(f1.getHealth()<1) {
+			f1.fighterDeath();
+		}
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -6,28 +6,74 @@ public class Test {
 		
 		Scanner input = new Scanner(System.in);
 		
-		Fighter f1 = new Fighter("A", 1, 1, 1, 1);
-				
-		Fighter[] allFighters = new Fighter[3];
+		System.out.println("Enter number of credits. For multiplayer option, enter at least 2 credits");
 		
-		for(int i=0; i<allFighters.length; i++) {
-			System.out.println("Unesi ime karaktera broj: " +(i+1) +".");
-			input.nextLine();
-			System.out.println("Unesi snagu karaktera: ");
-			int strength = input.nextInt();
-			System.out.println("Unesi brzinu karatera: ");
-			int speed = input.nextInt();
-			System.out.println("Unesi defanzivu karaktera: ");
-			int defence = input.nextInt();
-			int health = 100;
-			String name = input.nextLine();
-			allFighters[i] = new Fighter(name, strength, speed, defence, health);
+		int credits = input.nextInt();
+		
+		System.out.println("Choose your option: \n 1. Single player game \n 2. Multiplayer game");
+		int selectOption = input.nextInt();
+		while(selectOption!=1 && selectOption!=2) {
+			System.out.println("Invalid Option. Try again ");
+			selectOption = input.nextInt();
 		}
 		
-		for (int i = 0; i < allFighters.length; i++) {
-			System.out.println(allFighters[i].toString());
+		
+		Fighter[] fighter_options = new Fighter[5];
+		fighter_options[0] = new Fighter("Billy", 91, 90, 92, 100);
+		fighter_options[1] = new Fighter("Jimmy", 90, 91, 92, 100);
+		fighter_options[2] = new Fighter("Dalton", 92, 83, 79, 100);
+		fighter_options[3] = new Fighter("Abobo", 99, 56, 89, 100);
+		fighter_options[4] = new Fighter("Cheng Li", 82, 83, 91, 100);
+		
+		if(selectOption == 1) {
+			System.out.println("Select your character to fight against the computer");
+			System.out.println(" 1. Billy \n 2. Jimmy \n 3. Dalton \n 4. Abobo \n 5. Cheng LI");
+			int fighter_selected = input.nextInt();
+			while(fighter_selected < 1 || fighter_selected > 4) {
+				System.out.println("Invalid Option. Try again ");
+				fighter_selected = input.nextInt();
+			}
+			if(fighter_selected == 1) {
+				System.out.println("Your first battle is about to start..... ");
+				System.out.println("Billy is fighting Jimmy.");
+				
+				System.out.println("Commands: A - left \n D - right \n S - crouch \n W jump \n M - first kick");
+				
+				input.nextLine();
+				String command = input.nextLine();
+				
+				System.out.println(command);
+				while(
+					    !command.equals("A") &&
+					    !command.equals("S") &&
+					    !command.equals("D") &&
+					    !command.equals("W") &&
+					    !command.equals("M")
+					) {
+					command = input.nextLine();
+				}
+				if(command == "A") {
+					fighter_options[0].moveLeft();
+				}
+				else if(command == "D") {
+					fighter_options[0].moveRight();
+				}
+				else if(command.equals("S")) {
+					fighter_options[0].crouch();
+				}
+				else if(command.equals("W")) {
+					fighter_options[0].jump();
+				}
+				else if(command.equals("M")) {
+					System.out.println("LALA");
+					fighter_options[0].firstKick(fighter_options[1]);
+					if(!fighter_options[1].isInGuard()) {
+						fighter_options[0].calculateDamage(10, fighter_options[1]);
+						System.out.println(fighter_options[1].getHealth());
+					}
+				}
+			}
 		}
 	}
 
-	
 }
